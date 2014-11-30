266795b515e5e61494f874b324d5ba3b5d91e32f
Haojian <haojian@yahoo-inc.com>
2014-11-30 15:28:20 -0800

    update signal processing

--- signalprocessing/tracko_filter_2k6k2000uni_v2.m
+++ signalprocessing/tracko_filter_2k6k2000uni_v2.m
@@ -6,49 +6,17 @@ mf_left = flipud(upchirp(:));
 mf_right = flipud(downchirp(:));
 
 
-
 %for dev1
 leftchirpandright = csvread('dev1_matlabfile.txt');
 
-
-% pass filter start
-y = leftchirpandright;
-Fs = 48000;                    % Sampling frequency
-T = 1/Fs;                     % Sample time
-[L placeholder] = size(y);                     % Length of signal
-t = (0:L-1)*T;                % Time vector
-
-NFFT = 2^nextpow2(L); % Next power of 2 from length of y
-Y = fft(y,NFFT)/L;
-f = Fs/2*linspace(0,1,NFFT/2+1);
-
-% [b,a] = butter(9,0.09,'high');
-[b,a] = butter(9,[1500 7000]/24000);
-filt=filter(b,a,y);
-
-filted_Y = fft(filt,NFFT)/L;
-figure
-plot(f,2*abs(filted_Y(1:NFFT/2+1)), 'o-', f,2*abs(Y(1:NFFT/2+1)) ,'r-'); 
-legend('high pass filter','original')
-title('Single-Sided Amplitude Spectrum of y(t)')
-xlabel('Frequency (Hz)')
-ylabel('|Y(f)|')
-
-leftchirpandright = filt;
-% pass filter end
-
-
 left = abs(hilbert( filter(mf_left, 1, leftchirpandright ) )) ;
-% figure()
-% plot(left)
+figure()
+plot(left)
 
 right =  abs(hilbert( filter(mf_right, 1, leftchirpandright) ));
-% figure()
-% plot(right)
+figure()
+plot(right)
 
-% myfile = fopen( strcat(outputdir, '/dev1/filtered.txt'), 'w');
-% fprintf(myfile, '%g\n', filt)
-% fclose(myfile)
 
 myfile = fopen( strcat(outputdir, '/dev1/left.txt'), 'w');
 fprintf(myfile, '%g\n', left);
---
+++ signalprocessing/verify_freq.m
@@ -0,0 +1,3 @@
+upchirp = csvread('upchirp.txt');
+
+spectrogram(upchirp,256,250,256,48000,'yaxis') 
